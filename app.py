17adf5c2f71b11a864716b14bf63ad13ea80c16d
Hariprasath1911 <hari07011996@gmail.com>
2025-01-30 18:29:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pickle
+import numpy as np
 import pandas as pd
 import base64
 from sklearn.preprocessing import OrdinalEncoder
@@ -19,7 +20,7 @@ def set_background_image_local(image_path):
         <style>
         .stApp {{
             background-image: url("data:image/png;base64,{base64_image}");
-            background-size: cover;
+            background-size: contain;
             background-position: fit;
             background-repeat: repeat;
             background-attachment: fixed;
