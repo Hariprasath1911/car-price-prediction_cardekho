e5375edd54af1c2c0f5ea6b2a74c96e4982dbc8c
Hariprasath1911 <hari07011996@gmail.com>
2025-01-31 13:34:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,22 +37,9 @@ encoder=load_model("ordinal_encoder.pkl")
 ml_df=pd.read_excel("extracted_car_details.xlsx")
 st.title("Car Price Prediction App")
 
-car_city = ml_df["city"].unique().tolist()
-encoder.fit(np.array(car_city).reshape(-1, 1))
-car_ft = ml_df["ft"].unique().tolist()
-encoder.fit(np.array(car_ft).reshape(-1, 1))
-car_bt=ml_df["bt"].unique().tolist()
-encoder.fit(np.array(car_bt).reshape(-1, 1))
-car_transmission=ml_df["transmission"].unique().tolist()
-encoder.fit(np.array(car_transmission).reshape(-1, 1))
-car_oem=ml_df["oem"].unique().tolist()
-encoder.fit(np.array(car_oem).reshape(-1, 1))
-car_model=ml_df["model"].unique().tolist()
-encoder.fit(np.array(car_model).reshape(-1, 1))
-car_variantName=ml_df["variantName"].unique().tolist()
-encoder.fit(np.array(car_variantName).reshape(-1, 1))
-car_InsuranceValidity=ml_df["Insurance Validity"].unique().tolist()
-encoder.fit(np.array(car_InsuranceValidity).reshape(-1, 1))
+categorical_features = ["city", "ft", "bt", "transmission", "oem", "model", "variantName", "Insurance Validity"]
+encoder.fit(ml_df[categorical_features])
+dropdown_options = {feature: ml_df[feature].unique().tolist() for feature in categorical_features}
 
 tab1, tab2 = st.tabs(["Home", "Predict"])
 with tab1:
@@ -109,54 +96,61 @@ with tab2:
     a13,a14=st.columns(2)
     
     with a1:
-        city_select=st.selectbox("Select City",car_city)
+        city_select=st.selectbox("Select City",dropdown_options["city"])
     with a2:
-        ft_select=st.selectbox("Select fuel Type",car_ft)
+        ft_select=st.selectbox("Select fuel Type",dropdown_options["ft"])
     with a3:
-        bt_select=st.selectbox("Select Body Type",car_bt)
+        bt_select=st.selectbox("Select Body Type",dropdown_options["bt"])
     with a4:
         km=st.number_input("Enter KM driven",min_value=10)
     with a5:
-        transmission_select=st.selectbox("Select Body Type",car_transmission)
+        transmission_select=st.selectbox("Select Body Type",dropdown_options["transmission"])
     with a6:
         ownerNo=st.number_input("Enter no. of Owner's",min_value=1)
     with a7:
-        oem_select=st.selectbox("Select car manufacture name",car_oem)
+        oem_select=st.selectbox("Select car manufacture name",dropdown_options["oem"])
     with a8: 
-        model_select=st.selectbox("Select car Model name",car_model)
+        model_select=st.selectbox("Select car Model name",dropdown_options["model"])
     with a9:
         modelYear=st.number_input("Enter car manufacture year",min_value=1000)
     with a10:
-        variantName_select=st.selectbox("Select Model variant Name",car_variantName)
+        variantName_select=st.selectbox("Select Model variant Name",dropdown_options["variantName"])
     with a11:
         Registration_Year=st.number_input("Enter car registration year",min_value=1000)
     with a12:
-        InsuranceValidity_select=st.selectbox("Select Insurance Type",car_InsuranceValidity)
+        InsuranceValidity_select=st.selectbox("Select Insurance Type",dropdown_options["Insurance Validity"])
     with a13:
         Seats=st.number_input("Enter seat capacity",min_value=1)
     with a14:
         EngineDisplacement=st.number_input("Enter Engine CC",min_value=1)
         
     if st.button('Predict'):
-    
-        city = encoder.transform([[city_select]])[0][0]
-        ft=encoder.transform([[ft_select]])[0][0]
-        bt=encoder.transform([[bt_select]])[0][0]
-        transmission=encoder.transform([[transmission_select]])[0][0]
-        oem=encoder.transform([[oem_select]])[0][0]
-        model=encoder.transform([[model_select]])[0][0]
-        variantName=encoder.transform([[variantName_select]])[0][0]
-        InsuranceValidity=encoder.transform([[InsuranceValidity_select]])[0][0]
-        data = {
-            "city": city, "ft": ft, "bt": bt, "km": km,
-            "transmission": transmission, "ownerNo": ownerNo, "oem": oem,
-            "model": model, "modelYear": modelYear, "variantName": variantName,
-            "Registration Year": Registration_Year, "Insurance Validity": InsuranceValidity,
-            "Seats": int(Seats) if Seats else 5,
-            "Engine Displacement": float(EngineDisplacement) if EngineDisplacement else 799
+        encoded_inputs = {
+            "city": encoder.transform([[city_select]])[0][0],
+            "ft": encoder.transform([[ft_select]])[0][0],
+            "bt": encoder.transform([[bt_select]])[0][0],
+            "transmission": encoder.transform([[transmission_select]])[0][0],
+            "oem": encoder.transform([[oem_select]])[0][0],
+            "model": encoder.transform([[model_select]])[0][0],
+            "variantName": encoder.transform([[variantName_select]])[0][0],
+            "Insurance Validity": encoder.transform([[insurance_validity_select]])[0][0]
         }
-
-        input_data = pd.DataFrame([data])
+        input_data = pd.DataFrame([{
+            "city": encoded_inputs["city"],
+            "ft": encoded_inputs["ft"],
+            "bt": encoded_inputs["bt"],
+            "km": km,
+            "transmission": encoded_inputs["transmission"],
+            "ownerNo": ownerNo,
+            "oem": encoded_inputs["oem"],
+            "model": encoded_inputs["model"],
+            "modelYear": modelYear,
+            "variantName": encoded_inputs["variantName"],
+            "Registration Year": registration_year,
+            "Insurance Validity": encoded_inputs["Insurance Validity"],
+            "Seats": seats,
+            "Engine Displacement": engine_displacement
+        }])
         a=["km","ownerNo","modelYear","price","Registration Year","Seats","Engine Displacement"]
         for i in a:
             input_data[i]=np.cos(input_data[i])
