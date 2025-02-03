139283cb9b46d8d4852c756b599e02017e159ffd
Hariprasath1911 <hari07011996@gmail.com>
2025-02-03 18:09:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ with tab2:
         #a=["km","ownerNo","modelYear","Registration Year","Seats","Engine Displacement"]
         #for i in a:
             #input_data[i] = np.cos(input_data[[i]])
-        prediction = model_car.predict(input_data.values.reshape(1, -1))
+        prediction = model_car.predict(input_data)
                 
         st.subheader("Predicted Car Price")
         st.write(f"₹ {prediction}")
